sunnypilot/sunnypilot

sunnylink: consent confirmation dialog

selfdrive/ui/sunnypilot/layouts/settings/sunnylink.py
@@ -302,18 +302,26 @@ class SunnylinkLayout(Widget):
       self._restore_btn.set_text(tr("Restore Settings"))
 
   def _sunnylink_toggle_callback(self, state: bool):
-    if state:
-      description = tr(
-        "Welcome back!! We're excited to see you've enabled sunnylink again!")
-      color = rl.Color(0, 255, 0, 255)  # Green
-    else:
-      description = ("😢 " + tr("Not going to lie, it's sad to see you disabled sunnylink") +
-                     tr(", but we'll be here when you're ready to come back."))
-      color = rl.Color(255, 165, 0, 255)  # Orange
-    self._sunnylink_description.set_text(description)
-    self._sunnylink_description.set_color(color)
-    self._sunnylink_description.set_visible(True)
-    self._sunnylink_toggle.show_description(False)
+    if not state:
+      ui_state.params.put_bool("SunnylinkEnabled", False)
+      return
+
+    # Revert toggle until consent given
+    self._sunnylink_toggle.action_item.set_state(False)
+    ui_state.params.put_bool("SunnylinkEnabled", False)
+
+    def on_confirm(result):
+      if result == DialogResult.CONFIRM:
+        ui_state.params.put_bool("SunnylinkEnabled", True)
+        self._sunnylink_toggle.action_item.set_state(True)
+
+    dialog = ConfirmDialog(
+      tr("Sunnylink admins can potentially access your device state, settings, location, and github username. " +
+         "This data can be linked to your online account if paired."),
+      tr("Enable"),
+      tr("Cancel"),
+    )
+    gui_app.set_modal_overlay(dialog, callback=on_confirm)
 
   def _update_state(self):
     super()._update_state()

madmack/openpilot

selfdrive/ui/sunnypilot/layouts/settings/sunnylink.py
@@ -307,17 +307,38 @@ class SunnylinkLayout(Widget):
     sl_consent: bool = ui_state.params.get("CompletedSunnylinkConsentVersion") == sunnylink_consent_version
     sl_enabled: bool = ui_state.params.get_bool("SunnylinkEnabled")
 
+    def on_confirm(result):
+      enabled = result == DialogResult.CONFIRM
+      ui_state.params.put_bool("SunnylinkEnabled", enabled)
+      self._sunnylink_toggle.action_item.set_state(enabled)
+      self._update_description(enabled)
+
+    def show_warning():
+      dialog = ConfirmDialog(
+        tr("Sunnylink admins can potentially access your device state, settings, location, and github username. " +
+           "This data can be linked to your online account if paired."),
+        tr("Enable"),
+        tr("Cancel"),
+        callback=on_confirm,
+      )
+      gui_app.push_widget(dialog)
+
     if state and not sl_consent and not sl_enabled:
       def on_consent_done():
         enabled = ui_state.params.get_bool("SunnylinkEnabled")
         self._update_description(enabled)
         gui_app.pop_widget()
+        if enabled:
+          show_warning()
 
       sl_terms_dlg = SunnylinkConsentPage(done_callback=on_consent_done)
       gui_app.push_widget(sl_terms_dlg)
     else:
-      ui_state.params.put_bool("SunnylinkEnabled", state)
-      self._update_description(state)
+      if state:
+        show_warning()
+      else:
+        ui_state.params.put_bool("SunnylinkEnabled", False)
+        self._update_description(False)
 
   def _update_description(self, state: bool):
     if state: